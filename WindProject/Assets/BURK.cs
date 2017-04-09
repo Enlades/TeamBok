62e2b64ca77ec4f543ad6ef2e63c1164cefc7d1f
BurakPC\Burak <esadburak@yahoo.com.tr>
2017-04-09 16:40:57 +0300

    Zeynebin kodunu duzelttim

--- WindProject/Assets/BURK.cs
+++ WindProject/Assets/BURK.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class BURK : MonoBehaviour {
 
 	// Use this for initialization
-<<<<<<< HEAD
 
-=======
 	void Start () {
-		transform.Translate (Vector.zero);
+		transform.Translate (Vector3.zero);
 	}
->>>>>>> 91bfd64895fc2666773445cdf12452cf5347fe9c
 	
 	// Update is called once per frame
 	void Update () {
