3b984ef329da98acec9550e02ac3b2ad5db9b842
BurakPC\Burak <esadburak@yahoo.com.tr>
2017-04-09 16:34:21 +0300

    statrti sildim

--- WindProject/Assets/BURK.cs
+++ WindProject/Assets/BURK.cs
@@ -4,9 +4,7 @@ using System.Collections;
 public class BURK : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
