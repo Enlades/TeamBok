88eca6acad703f61b98e44a0e4cd0b26b6b9737a
Enlades <muratttkaan@gmail.com>
2017-04-26 18:50:17 +0300

    WindParticle Script

---
+++ WindProject/Assets/WindParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// wind
+/// </summary>
+public class WindParticle : MonoBehaviour {
+    public float windParticlePower;
+    public Vector3 vector;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        GetComponent<AreaEffector2D>().forceAngle = Vector2.Angle(Vector2.right, vector) * -1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(vector*speed * Time.deltaTime);
+        GetComponent<AreaEffector2D>().forceMagnitude = windParticlePower;
+        
+	}
+}
